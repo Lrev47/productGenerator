f96da76aba9ca86ace12564b4e7195fbbba6fb21
luis revilla <lrev4766@gmail.com>
2024-12-31 15:28:07 -0600

    combined app and server into server

--- scr/server.js
+++ scr/server.js
@@ -1,21 +1,23 @@
-// scr/server.js
+// server.js
 require("dotenv").config();
-
-// 1. Import express and path here:
 const express = require("express");
 const path = require("path");
-
-const app = require("./app");
+const routes = require("./routes");
 const { port } = require("./config");
 
-// 2. Serve the public folder before any other routes
+// Create Express instance
+const app = express();
+
+// JSON parsing middleware
+app.use(express.json());
+
+// Serve static files
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-// 3. Mount any additional routes after
-const imageRoutes = require("./routes/imageRoutes");
-app.use("/api/images", imageRoutes);
+// Mount routes
+app.use("/api", routes);
 
-// Finally, start the server
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
